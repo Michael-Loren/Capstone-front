2c0528381dd3211208517ac1b4e9d9895b67e67b
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-25 14:07:34 -0500

    new

--- capstone-front/src/Pages/CartPage.js
+++ capstone-front/src/Pages/CartPage.js
@@ -1,7 +1,70 @@
-import React from "react"
+import React from "react";
 
-export default function CartPage () {
-    return(
-        <h1>Cart Page</h1>
-    )
+export default function CartPage() {
+  return (
+    <div>
+      <h4>Shopping Cart</h4>
+      <div className="container-sm">
+        <div className="col">
+          <div className="card-group">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Menu Item</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Price</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Quantity</h5>
+              </div>
+            </div>
+          </div>
+          <div className="card-group">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Menu Item</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Price</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Quantity</h5>
+              </div>
+            </div>
+          </div>
+          <div className="card-group">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Menu Item</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Price</h5>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Quantity</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- capstone-front/src/Pages/HomePage.js
+++ capstone-front/src/Pages/HomePage.js
@@ -1,8 +1,12 @@
-import React from "react"
-import MenuItems from "../Components/MenuItems"
+import React from "react";
+import MenuItems from "../Components/MenuItems";
+import SideMenu from "../Components/SideMenu";
 
-export default function HomePage(){
-    return(
-        <MenuItems/>
-    )
+export default function HomePage() {
+  return (
+    <div>
+      <SideMenu />
+      <MenuItems />
+    </div>
+  );
 }
