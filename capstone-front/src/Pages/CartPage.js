6d968ce7965950682742ec3958db9e32fff9a2aa
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-26 15:03:33 -0500

    finished CartPage

--- capstone-front/src/Pages/CartPage.js
+++ capstone-front/src/Pages/CartPage.js
@@ -3,67 +3,41 @@ import React from "react";
 export default function CartPage() {
   return (
     <div>
-      <h4>Shopping Cart</h4>
-      <div className="container-sm">
-        <div className="col">
-          <div className="card-group">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Menu Item</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Price</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Quantity</h5>
-              </div>
-            </div>
-          </div>
-          <div className="card-group">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Menu Item</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Price</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Quantity</h5>
-              </div>
-            </div>
-          </div>
-          <div className="card-group">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Menu Item</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Price</h5>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Quantity</h5>
-              </div>
-            </div>
+      <h1>Shopping Cart</h1>
+      <div class="container p-4 my-5 bg-secondary text-black rounded">
+        <div class="container-sm  bg-light">
+          <div class="row">
+            <div class="col text-center h2">Menu Item</div>
+            <div class="col text-center h2">Price</div>
+            <div class="col  text-center h2">Quantity</div>
           </div>
         </div>
+      </div>
+      <div class="container p-4 my-5 bg-secondary text-black rounded">
+        <div class="container-md  bg-light">
+          <div class="row">
+            <div class="col text-center h2">Menu Item</div>
+            <div class="col text-center h2">Price</div>
+            <div class="col text-center h2">Quantity</div>
+          </div>
+        </div>
+      </div>
+      <div class="container p-4 my-5 bg-secondary text-black rounded">
+        <div class="container-md bg-light">
+          <div class="row">
+            <div class="col text-center h2">Menu Item</div>
+            <div class="col text-center h2">Price</div>
+            <div class="col text-center h2">Quantity</div>
+          </div>
+        </div>
+      </div>
+      <div className="ms-5 ps-5 ">
+        <h2>Total Price: $0.00 </h2>
+      </div>
+      <div className="d-grid gap-2 d-md-flex justify-content-md-endd-grid gap-2 d-md-flex justify-content-md-end me-5 pe-5">
+        <button className="btn btn-primary btn-lg" type="button">
+          Checkout
+        </button>
       </div>
     </div>
   );
