da899aaf1b2f133500020bee83af59dd0afbd546
Anthony Sanchez <antsanchez198@gmail.com>
2022-01-26 20:24:09 -0500

    filter menu

--- capstone-front/src/Components/SideMenu.js
+++ capstone-front/src/Components/SideMenu.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { useState } from "react"
 
-export default function SideMenu ({allFoods}) {
+export default function SideMenu (props) {
 
+    const allFoods = props.allFoods
+    const setType = props.setType
     const [categories, setCategories] = useState([])
-
-    console.log(allFoods)
     
     allFoods.map(index => {
         if(!categories.includes(index.f_category)){
@@ -13,14 +13,20 @@ export default function SideMenu ({allFoods}) {
         }}
     )
 
+    // const setFoodType = async (foodType) => {
+    //     setType(`/${foodType}`)
+    //     console.log(foodType)
+    // }
+
     return(
     <div className="sideMenu"> 
             <div class="d-flex align-items-start ">
             <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
             
             {categories.map(index =>{
+            //const currentCat = index.f_category;
              return(
-                <button class="nav-link" data-bs-toggle="pill">{index}</button>
+                <button class="nav-link" data-bs-toggle="pill" onClick={() => setType(index)}>{index}</button>
         )})} 
         </div>
         </div> 
--- capstone-front/src/Pages/HomePage.js
+++ capstone-front/src/Pages/HomePage.js
@@ -6,11 +6,12 @@ import { useState, useEffect } from "react";
 
 export default function HomePage() {
 
-  const [foodItems, setFoodItems] = useState([])
+  const [type, setType] = useState("")
+  const [foodItems, setFoodItems] = useState([]);
 
   const fetchFoodData = async () => {
       try {
-          const response = await fetch("http://localhost:5000/Food");
+          const response = await fetch(`http://localhost:5000/Food/${type}`);
           const jsonData = await response.json();
           setFoodItems(jsonData);
           console.log(foodItems)
@@ -21,12 +22,14 @@ export default function HomePage() {
 
   useEffect(()=>{
       fetchFoodData()
-  },[])
+  },[type])
+
+  console.log(type)
 
 
   return (
     <div>
-      <SideMenu allFoods = {foodItems}/>
+      <SideMenu  allFoods = {foodItems} setType = {setType}/>
       <MenuItems allFoods = {foodItems}/>
     </div>
   );
