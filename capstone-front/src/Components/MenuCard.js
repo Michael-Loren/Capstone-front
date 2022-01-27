3381e3044a9d82480032ee25de595a5f711dd454
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-27 11:30:03 -0500

    add cart and total

--- capstone-front/src/Components/MenuCard.js
+++ capstone-front/src/Components/MenuCard.js
@@ -1,28 +1,34 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 import MenuItems from "./MenuItems";
 
 export default function MenuCard({ item }) {
+  let [inCart, setInCart] = useState(false);
 
-    let [inCart, setInCart] = useState(false);
-
-    const addToCart = async e => {    
-        setInCart(!inCart);
-        if(!inCart){
-        e.target.classList.replace(e.target.classList[4],"btn-success");}
-        else{
-        e.target.classList.replace(e.target.classList[4],"btn-warning");
-        }
+  const addToCart = async (e) => {
+    setInCart(!inCart);
+    if (!inCart) {
+      e.target.classList.replace(e.target.classList[4], "btn-success");
+    } else {
+      e.target.classList.replace(e.target.classList[4], "btn-warning");
     }
+  };
 
-    return(
-      <div class="card">
+  return (
+    <div class="card">
       <div class="container">
-        <h4><b className="cardItem">{item.f_name}</b></h4>
+        <h4>
+          <b className="cardItem">{item.f_name}</b>
+        </h4>
         <div className="cardItem">{item.f_desc}</div>
         <p className="inner cardItem bottom">{item.f_price}</p>
-        <button className="bi bi-cart cardBtn btn btn-warning bottom" onClick={e => addToCart(e)}>{inCart ? " Added" : " Add to Cart"}</button>
+        <button
+          className="bi bi-cart cardBtn btn btn-warning bottom"
+          onClick={(e) => addToCart(e)}
+        >
+          {inCart ? " Added" : " Add to Cart"}
+        </button>
       </div>
-      </div>
-    )
+    </div>
+  );
 }
